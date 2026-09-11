3768f91b6e597f588dd294d55dd1437337e02afa
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-09-11 17:48:31 +0300

    add calc app

---
+++ practice/app.js
@@ -0,0 +1,58 @@
+// const element = document.getElementById("ele");
+
+// element.textContent = "Hello1";
+
+// let items = ["PS5", "XBOX", "Keyboard"];
+// let colors = ["red", "Green", "Blue"];
+
+// mainLoop: for (let i = 0; i < items.length; i++) {
+//   //   console.log(`=`.repeat(12));
+//   console.log(`${items[i]}`);
+//   nestedLoop: for (let j = 0; j < colors.length; j++) {
+//     console.log(`--- ${colors[j]}`);
+//     if (colors[j] === "Green") {
+//       break mainLoop;
+//     }
+//   }
+//   //   console.log(`=`.repeat(12));
+// }
+// Coder
+// React - js Developer
+// Software engineer
+
+// let myName = "Jaya Ahmed";
+// newStr = myName.split("");
+// console.log(newStr);
+
+// number + methods
+// bool + methods
+// string + methods
+// array + methods
+// isNaN()
+// ?? ||
+// function 
+// challenges for every week
+
+// function calculator(num1, num2, operator) {
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     if (num2 === 0) {
+//       return console.error("Error Cannor divide by 0");
+//     }
+//     return num1 / num2;
+//   } else {
+//     return "invalid Operatrion";
+//   }
+// }
+//  % ***
+
+// console.log(calculator(10, 5, "+"));
+// console.log(calculator(10, 5, "-"));
+// console.log(calculator(10, 5, "*"));
+// console.log(calculator(10, 5, "/"));
+// console.log(calculator(10, 0, "/"));
