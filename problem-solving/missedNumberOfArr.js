a31a56ebe0a1761a944442995005b6b74e2e9fbe
jaihhamd <jayahemdan7@gmail.com>
2026-09-22 16:20:07 +0300

    Missed Number in array

---
+++ problem-solving/missedNumberOfArr.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 6, 7, 9];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  if (arr[i + 1] - arr[i] == 1) {
+    // console.log(arr[i]);
+  } else if (arr[i + 1] - arr[i] !== 1) {
+    console.log(`The missing numbers are  ${arr[i] + 1}`);
+  }
+}
