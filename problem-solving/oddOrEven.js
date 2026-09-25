45d5d7c69706843456e1ec410a7729c04bdaa3a1
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-09-25 23:51:11 +0300

    review all first sums

--- problem-solving/oddOrEven.js
+++ problem-solving/oddOrEven.js
@@ -1,8 +1,12 @@
 function evenOrOddNumber(num) {
-  if (num % 2 == 0) {
-    return "even"
+  if (num % 2 === 0) {
+    return "even";
+  } else if (num % 2 === 1) {
+    return "Odd";
+  } else if (num % 2 !== 0 || num % 2 === 1) {
+    return "It may be Even or Odd depends on sum but the summation doesn't reconize it";
   } else {
-    return "odd"
+    return "Zero";
   }
 }
 console.log(evenOrOddNumber(5));
--- problem-solving/theFirstNumAfter0.js
+++
@@ -1,7 +0,0 @@
-let num = [1, 2, 8, 0 , 4, 3, 10, 7, 5];
-for (let i = 0; i < num.length; i++) {
-  if (num[i] == 0) {
-    console.log(num[i - 1]);
-    console.log(num[i + 1]);
-  }
-}
